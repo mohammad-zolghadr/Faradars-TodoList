d3c930819891fc89051672acb105736e7e5d6a1a
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2023-01-12 09:58:50 +0330

    S0304-01

--- src/components/ListOfTodos.js
+++ src/components/ListOfTodos.js
@@ -1,7 +1,29 @@
 import React from "react";
 
+import style from "./ListOfTodos.module.css";
+import emptyImage from "../assets/images/emptyList.svg";
+import Work from "./Work";
+import { Link } from "react-router-dom";
+
 const ListOfTodos = () => {
-  return <div>List Of Todos</div>;
+  const loadedItems = [];
+  return (
+    <div className={style.wlContainer}>
+      {loadedItems.length > 0 ? (
+        loadedItems.map((item) => {
+          return <Work />;
+        })
+      ) : (
+        <div className={style.wlEmptyList}>
+          <img src={emptyImage} />
+          <p>لیست کارهات خالیه دوست من</p>
+          <Link to="/add-todo">
+            <button>اضافه کردن کار جدید</button>
+          </Link>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ListOfTodos;
